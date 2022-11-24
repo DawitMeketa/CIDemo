ba680595eb60ff74a11c1b048281c369d7da4217
Dawit Meketa <meketadawit@gmail.com>
2022-11-23 19:11:42 -0600

    comment added second

--- DemoApp/src/main/java/com/example/demoapp/DemoAppApplication.java
+++ DemoApp/src/main/java/com/example/demoapp/DemoAppApplication.java
@@ -2,9 +2,7 @@ package com.example.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
@@ -17,9 +15,19 @@ public class DemoAppApplication {
         SpringApplication.run(DemoAppApplication.class, args);
     }
 
-
+// comment addes
     @GetMapping
     public String getData(){
         return "the thing has worked so no need to worry";
+    }
+
+    @PostMapping
+    public void done(@RequestBody String s){
+
+    }
+
+    @PostMapping("/demo")
+    public void done2(@RequestBody String s){
+
     }
 }
